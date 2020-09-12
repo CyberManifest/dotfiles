80f510ed6286334bb11617c84aafb4b7a67973a0
CyberManifest <ryan.k.mckee@gmail.com>
2020-09-12 03:38:38 -0500

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,5 @@ fortune
 echo "going to list the contents of the directory..."
 ls -la
 echo "this line will get deleted..."
-printf '$-1,$d\nw\nq\n' | ed -s ~/test.sh
+printf '$-2,$d\nw\nq\n' | ed -s ~/test.sh
+exit 0
