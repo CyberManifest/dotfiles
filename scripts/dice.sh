8788c7ba5d4ce602d3dc1faaac561699837cabe4
CyberManifest <ryan.k.mckee@gmail.com>
2020-09-28 08:42:06 -0500

    Update dice.sh

--- scripts/dice.sh
+++ scripts/dice.sh
@@ -1,4 +1,4 @@
-! /bin/bash
+#! /bin/bash
 #Roll dice using bash
 
 roll=$1
@@ -6,11 +6,12 @@ for((i=0;i<roll;i++))
 do
 die=$(($RANDOM % 6 + 1))
 case $die in
-        1) echo "⚀";;
-        2) echo "⚁";;
-        3) echo "⚂";;
-        4) echo "⚃";;
-        5) echo "⚄";;
-        6) echo "⚅";;
+        1) echo -n " ⚀ ";;
+        2) echo -n " ⚁ ";;
+        3) echo -n " ⚂ ";;
+        4) echo -n " ⚃ ";;
+        5) echo -n " ⚄ ";;
+        6) echo -n " ⚅ ";;
 esac
 done
+echo
